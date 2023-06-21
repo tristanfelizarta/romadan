9c54f5cadff29e16b779e2995db757290c85c732
Tan <m.tristanfelizarta@gmail.com>
2023-06-21 09:30:14 +0800

    chore: add image domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    images: {
+        domains: [
+            'res.cloudinary.com',
+            'avatars.githubusercontent.com',
+            'lh3.googleusercontent.com'
+        ]
+    }
 }
 
 module.exports = nextConfig
